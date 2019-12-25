5dbc6d99b26dd385b312eb47fbf5cf0af03c1fd3
Lindo21 <Lindon.Ngonga@zamstats.gov.zm>
2019-12-25 09:14:07 +0200

    Add project files.

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using ConsoleUtilities;
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Utilities utilities = new Utilities();
+            string stringOutputText = "Hello world";
+            utilities.writeline(stringOutputText);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ UnitTestConsoleUtilities/UtilitiesTests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleUtilities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleUtilities.Tests
+{
+    [TestClass()]
+    public class UtilitiesTests
+    {
+        [TestMethod()]
+        public void writelineTest()
+        {
+            //Arrange
+            Utilities utilities = new Utilities();
+            string stringInputText = "Hello world";
+            //Act
+            string output = utilities.writeline(stringInputText);
+            //Assert
+            Assert.AreEqual(stringInputText, output);
+
+
+
+        }
+    }
+}
